da75b6aa5effc7040b75811ab0d337eb3534b982
jongwon <bluesinger75@gmail.com>
2023-08-11 18:39:00 +0900

    fixed untracked files

---
+++ build/generated/querydsl/com/bobfriend/bobfriend_bend/entity/QVideo.java
@@ -0,0 +1,39 @@
+package com.bobfriend.bobfriend_bend.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QVideo is a Querydsl query type for Video
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QVideo extends EntityPathBase<Video> {
+
+    private static final long serialVersionUID = -133709071L;
+
+    public static final QVideo video = new QVideo("video");
+
+    public final StringPath title = createString("title");
+
+    public final StringPath videoId = createString("videoId");
+
+    public QVideo(String variable) {
+        super(Video.class, forVariable(variable));
+    }
+
+    public QVideo(Path<? extends Video> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QVideo(PathMetadata metadata) {
+        super(Video.class, metadata);
+    }
+
+}
+
